be23dc7380f322c8d4ec11d6d43e25bbd9282ee2
201710757 <neti69@naver.com>
2021-04-30 00:50:42 +0900

    search face & draw box <- video

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-import os
-import pandas as pd
-import numpy as np
 import cv2
-from tqdm import tqdm_notebook as tqdm
-from matplotlib import pyplot as plt
-from PIL import Image
 import torch
-import time
+import numpy as np
+from facenet_pytorch import MTCNN
+from tqdm import tqdm_notebook as tqdm
+import os
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-from facenet_pytorch import MTCNN
-detector = MTCNN(device=device)
-
-def detect_facenet_pytorch(detector, images, batch_size=20):
-    faces = []
-    for lb in np.arange(0, len(images), batch_size):
-        imgs_pil = [Image.fromarray(image) for image in images[lb:lb+batch_size]]
-        faces.extend(detector(imgs_pil))
-    return faces
+mtcnn = MTCNN(device=device)
 
 capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-batch_size = 20
 while cv2.waitKey(33) != ord('q'):
     ret, frame = capture.read()
-    faces = detect_facenet_pytorch(detector, frame, batch_size)
-    cv2.imshow("FACE", faces)
-
+    faces, _ = mtcnn.detect(frame)
+    #print(faces)
+    try:    
+        for face in faces:
+            face = np.trunc(face)
+            cv2.rectangle(frame, (face[0],face[1]), (face[2], face[3]), (255,0,0), 3)
+        
+    except:
+        pass
+    cv2.imshow("FACE", frame)
 capture.release()
 cv2.destroyAllWindows()
