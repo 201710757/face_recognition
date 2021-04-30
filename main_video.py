a2cb8060a73990e39e5febd888e3b5034e3a4776
201710757 <neti2207@gmail.com>
2021-05-01 01:00:06 +0900

    MTCNN Fast&normal option

--- main_video.py
+++ main_video.py
@@ -18,20 +18,24 @@ capture = cv2.VideoCapture(0)
 while cv2.waitKey(33) != ord('q'):
     ret, frame = capture.read()
     
-    frames = [] #Fast Versio
+    frames = []             #Fast Version
     frames.append(frame)    #Fast Version
     
-    faces, _ = mtcnn.detect(frame) if not FastVersion else mtcnn(frames), 0
-    print(faces)
+    faces, _ = mtcnn.detect(frame) if not FastVersion else [mtcnn(frames), 0]
+    # print(faces)
     
     try:    
         for face in faces:
             face = np.trunc(face)
-            print("RECT : {}, {}, {}, {}".format(face[0],face[1],face[2],face[3]))
-            cv2.rectangle(frame, (int(face[0]),int(face[1])), (int(face[2]), int(face[3])), (255,0,0), 3)
-        
+            # print("RECT : {}, {}, {}, {}".format(face[0],face[1],face[2],face[3]))
+            lu_x, lu_y, rd_x, rd_y  = int(face[0]), int(face[1]), int(face[2]), int(face[3])
+            cv2.rectangle(frame, (lu_x, lu_y), (rd_x, rd_y), (255,0,0), 3)
+            #cv2.rectangle(frame, (int(face[0]),int(face[1])), (int(face[2]), int(face[3])), (255,0,0), 3)
+                
     except Exception as e:
         print("Err : ", e)
+    
     cv2.imshow("FACE", frame)
+
 capture.release()
 cv2.destroyAllWindows()
